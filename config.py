c7e0e3a7e0b1676496edddd660a52a52ccafe53a
saidkamolisakhonov-alt <saidkamolisakhonov@gmail.com>
2026-02-03 23:36:34 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,7 @@
 import os
 
-TOKEN = os.getenv("BOT_TOKEN")
-ADMIN_ID = 5328760224
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+ADMIN_ID = int(os.getenv("ADMIN_ID", 0))
+
+if not BOT_TOKEN:
+    raise RuntimeError("BOT_TOKEN is not set in Railway Variables")
